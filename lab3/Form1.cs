74df27a6d86d37fd7d50e7c96fd76d10e7271994
rsoylon <r.soylon.526044@umindanao.edu.ph>
2025-04-22 09:50:13 +0800

    add

--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,20 @@
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+    }
 }
