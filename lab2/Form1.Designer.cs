729c833e55230484b0fa5a8cb79fc164630c8c1b
rsoylon <r.soylon.526044@umindanao.edu.ph>
2025-04-22 09:39:51 +0800

    add

--- lab2/Form1.Designer.cs
+++ lab2/Form1.Designer.cs
@@ -20,20 +20,99 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonCalculateFactorial = new Button();
+            lblResult = new Label();
+            txtInput = new TextBox();
+            btnCalculateSum = new Button();
+            txtArrayInput = new TextBox();
+            lblSumResult = new Label();
+            SuspendLayout();
+            // 
+            // buttonCalculateFactorial
+            // 
+            buttonCalculateFactorial.Location = new Point(331, 273);
+            buttonCalculateFactorial.Name = "buttonCalculateFactorial";
+            buttonCalculateFactorial.Size = new Size(178, 39);
+            buttonCalculateFactorial.TabIndex = 0;
+            buttonCalculateFactorial.Text = "Calculate Factorial";
+            buttonCalculateFactorial.UseVisualStyleBackColor = true;
+            buttonCalculateFactorial.Click += button1_Click;
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(243, 218);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(57, 25);
+            lblResult.TabIndex = 2;
+            lblResult.Text = "Label";
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(243, 156);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(100, 33);
+            txtInput.TabIndex = 3;
+            // 
+            // btnCalculateSum
+            // 
+            btnCalculateSum.Location = new Point(331, 345);
+            btnCalculateSum.Name = "btnCalculateSum";
+            btnCalculateSum.Size = new Size(178, 39);
+            btnCalculateSum.TabIndex = 4;
+            btnCalculateSum.Text = "Calculate Sum";
+            btnCalculateSum.UseVisualStyleBackColor = true;
+            btnCalculateSum.Click += btnCalculateSum_Click;
+            // 
+            // txtArrayInput
+            // 
+            txtArrayInput.Location = new Point(411, 156);
+            txtArrayInput.Name = "txtArrayInput";
+            txtArrayInput.Size = new Size(100, 33);
+            txtArrayInput.TabIndex = 5;
+            // 
+            // lblSumResult
+            // 
+            lblSumResult.AutoSize = true;
+            lblSumResult.Location = new Point(411, 218);
+            lblSumResult.Name = "lblSumResult";
+            lblSumResult.Size = new Size(63, 25);
+            lblSumResult.TabIndex = 6;
+            lblSumResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblSumResult);
+            Controls.Add(txtArrayInput);
+            Controls.Add(btnCalculateSum);
+            Controls.Add(txtInput);
+            Controls.Add(lblResult);
+            Controls.Add(buttonCalculateFactorial);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button buttonCalculateFactorial;
+        private Label lblResult;
+        private TextBox txtInput;
+        private Button btnCalculateSum;
+        private TextBox txtArrayInput;
+        private Label lblSumResult;
+    }
 }
--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -1,10 +1,40 @@
 namespace lab2
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int RecursiveFactorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * RecursiveFactorial(n - 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txtInput.Text);
+            int result = RecursiveFactorial(number);
+            lblResult.Text = $"Factorail: {result}";
+        }
+        public int RecursiveSum(int[] numbers, int n)
+        {
+            if (n == 0) return numbers[0];
+            return numbers[n] * RecursiveFactorial(n - 1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculateSum_Click(object sender, EventArgs e)
+        {
+            int[] numbers = txtArrayInput.Text.Split(',').Select(int.Parse).ToArray();
+            int result = RecursiveSum(numbers, numbers.Length - 1);
+            lblSumResult.Text = $"Sum: {result}";
+          }
+    }
 }
